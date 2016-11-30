723b9d801538d7de0ba713abd4513654114648d1
Neil Wiborg <NeilWiborg@outlook.com>
2016-11-29 16:25:33 -0800

    Switch Setup

---
+++ ConsoleCalculator/Character.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleCalculator.Helper;
+
+namespace ConsoleCalculator
+{
+    class Character
+    {
+        string menuSelector;
+        int playerInput;
+        protected MENUS menu;
+        //Getters
+        public MENUS GetMenu()
+        {
+            return menu;
+        }
+        //Menus
+        public void MenuMain()
+        {
+            Console.WriteLine("Welcome to Console Calculator! What can I do for you today?");
+            Console.WriteLine();
+            Console.WriteLine("[1] Decimal Math");
+            Console.WriteLine("[2] Binary Math");
+            Console.WriteLine("[3] Hexadecimal Math");
+            Console.WriteLine("[4] Logic Gate Solver");
+            Console.WriteLine("[5] Shooter/Target Solver");
+            Console.WriteLine();
+            Console.WriteLine("[0] Close Console Calculator");
+        }
+
+        public void MenuDecimal()
+        {
+            Console.Clear();
+            Console.WriteLine("You are now in the Decimal Math section. Select an option below.");
+            Console.WriteLine();
+            Console.WriteLine("[1] Enter Expressions");
+            Console.WriteLine("[2] Decimal to Binary Conversion");
+            Console.WriteLine("[3] Decimal to Hexadecimal Conversion");
+            Console.WriteLine();
+            Console.WriteLine("[~] Back to Main Menu");
+            menuSelector = Console.ReadLine();
+            if (menuSelector == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("Enter expressions below:");
+                Console.WriteLine();
+                Console.ReadLine();
+                //playerInput = EvaluateExpression(math);
+                //Console.WriteLine(playerInput);
+            }
+            if (menuSelector == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("Enter decimal number below.");
+                Console.WriteLine();
+                Console.WriteLine("Input:");
+                playerInput = Convert.ToInt32(Console.ReadLine());
+                string playerAnswer = Convert.ToString(playerInput, 2);
+                Console.WriteLine("Answer:");
+                Console.WriteLine(playerAnswer);
+                Console.ReadKey();
+            }
+            if (menuSelector == "3")
+            {
+                Console.Clear();
+                Console.WriteLine("Enter decimal number below.");
+                Console.WriteLine();
+                Console.WriteLine("Input:");
+                playerInput = Convert.ToInt32(Console.ReadLine());
+                string playerAnswer = Convert.ToString(playerInput, 16);
+                Console.WriteLine("Answer:");
+                Console.WriteLine(playerAnswer);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+                Console.ReadKey();
+            }
+            Console.ReadKey();
+        }
+    
+    }
+}
--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -12,68 +12,22 @@ namespace ConsoleCalculator
     {
         static void Main(string[] args)
         {
-            int playerInput = 0;
             string menuSelector;
-            Console.WriteLine("Welcome to Console Calculator! What can I do for you today?");
-            Console.WriteLine();
-            Console.WriteLine("[1] Decimal Math");
-            Console.WriteLine("[2] Binary Math");
-            Console.WriteLine("[3] Hexadecimal Math");
-            Console.WriteLine("[4] Logic Gate Solver");
-            Console.WriteLine("[5] Shooter/Target Solver");
-            Console.WriteLine();
-            Console.WriteLine("[0] Close Console Calculator");
-            menuSelector = Console.ReadLine();
-            if(menuSelector == "1")
+            Character Player = new Character();
+            int playerInput = 0;
+            switch(Player.GetMenu())
             {
-                Console.Clear();
-                Console.WriteLine("You are now in the Decimal Math section. Select an option below.");
-                Console.WriteLine();
-                Console.WriteLine("[1] Enter Expressions");
-                Console.WriteLine("[2] Decimal to Binary Conversion");
-                Console.WriteLine("[3] Decimal to Hexadecimal Conversion");
-                Console.WriteLine();
-                Console.WriteLine("[~] Back to Main Menu");
-                menuSelector = Console.ReadLine();
-                if(menuSelector == "1")
-                {
-                    Console.Clear();
-                    Console.WriteLine("Enter expressions below:");
-                    Console.WriteLine();
-                    Console.ReadLine();
-                    //playerInput = EvaluateExpression(math);
-                    //Console.WriteLine(playerInput);
-                }
-                if (menuSelector == "2")
-                {
-                    Console.Clear();
-                    Console.WriteLine("Enter decimal number below.");
-                    Console.WriteLine();
-                    Console.WriteLine("Input:");
-                    playerInput = Convert.ToInt32(Console.ReadLine());
-                    string playerAnswer = Convert.ToString(playerInput, 2);
-                    Console.WriteLine("Answer:");
-                    Console.WriteLine(playerAnswer);
-                    Console.ReadKey();
-                }
-                if (menuSelector == "3")
-                {
-                    Console.Clear();
-                    Console.WriteLine("Enter decimal number below.");
-                    Console.WriteLine();
-                    Console.WriteLine("Input:");
-                    playerInput = Convert.ToInt32(Console.ReadLine());
-                    string playerAnswer = Convert.ToString(playerInput, 16);
-                    Console.WriteLine("Answer:");
-                    Console.WriteLine(playerAnswer);
-                    Console.ReadKey();
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input!");
-                    Console.ReadKey();
-                }
-                Console.ReadKey();
+                default:
+                case MENUS.MMAIN:
+                    Player.MenuMain();
+                    break;
+                case MENUS.MDEC:
+                    Player.MenuDecimal();
+                    break;
+            }
+            menuSelector = Console.ReadLine();
+            if (menuSelector == "1")
+            {
             }
             else if (menuSelector == "2")
             {
@@ -192,7 +146,7 @@ namespace ConsoleCalculator
             }
             else if (menuSelector == "0")
             {
-                
+                Environment.Exit(0);
             }
             else
             {
